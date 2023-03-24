1d7407fc632f280a3a6f975b7d6fb95aef11d8e8
Michello <michellorayhan14@gmail.com>
2023-03-25 00:21:56 +0700

    update-11
    perubahan karakter page dan info page

--- info-js.js
+++ info-js.js
@@ -10,6 +10,8 @@ var counter = 0
 
 window.onload = function(){
     counter_check()
+    counterf_check()
+    countercg_check()
 };
 
 bvideo_1.addEventListener("click", function(){
@@ -79,8 +81,132 @@ function counter_check(){
     }
 }
 
+let fbutton_1 = document.getElementById('fitur-btn-left')
+let fbutton_2 = document.getElementById('fitur-btn-right')
+const slide_fitur = document.querySelectorAll('.slide-fitur-grid')
+var counter_f =0
+const f_opacity = fbutton_2.style.opacity
 
 
+slide_fitur.forEach(
+    (slide, index)=>{
+        slide.style.left = `${index*100}%`
+    }
+)
+
+fbutton_1.addEventListener("click", function(){
+    if(counter_f>0){
+        counter_f--
+        slideImage_fitur()
+        counterf_check()
+    }    
+})
+
+fbutton_2.addEventListener("click", function(){
+    if(counter_f<4){
+        counter_f++
+        slideImage_fitur()
+        counterf_check()
+    }    
+})
+
+const slideImage_fitur = () =>{
+    slide_fitur.forEach(
+        (slide) => {
+            slide.style.transform = `translateX(-${counter_f*100}%)`
+        }
+    )
+}
+
+function counterf_check(){
+    if(counter_f>0 && counter_f<4){
+        fbutton_1.style.opacity = f_opacity
+        fbutton_2.style.opacity = f_opacity
+    }
+    if(counter_f==0){
+        fbutton_1.style.opacity = 0.1
+    }
+    if(counter_f==4){
+        fbutton_2.style.opacity = 0.1
+    }
+}
 
 
+let cgbutton_1 = document.getElementById('cuplikan-btn-left')
+let cgbutton_2 = document.getElementById('cuplikan-btn-right')
+const slide_cg = document.querySelectorAll('.slide-cuplikan-grid')
+var counter_cg =0
+const cg_opacity = cgbutton_2.style.opacity
 
+slide_cg.forEach(
+    (slide, index)=>{
+        slide.style.left = `${index*100}%`
+    }
+)
+
+cgbutton_1.addEventListener("click", function(){
+    if(counter_cg>0){
+        counter_cg--
+        slideImage_cg()
+        countercg_check()
+    }    
+})
+
+cgbutton_2.addEventListener("click", function(){
+    if(counter_cg<3){
+        counter_cg++
+        slideImage_cg()
+        countercg_check()
+    }    
+})
+
+const slideImage_cg = () =>{
+    slide_cg.forEach(
+        (slide) => {
+            slide.style.transform = `translateX(-${counter_cg*100}%)`
+        }
+    )
+}
+
+function countercg_check(){
+    if(counter_cg>0 && counter_cg<3){
+        cgbutton_1.style.opacity = cg_opacity
+        cgbutton_2.style.opacity = cg_opacity
+    }
+    if(counter_cg==0){
+        cgbutton_1.style.opacity = 0.1
+    }
+    if(counter_cg==3){
+        cgbutton_2.style.opacity = 0.1
+    }
+}
+
+
+let video_g = document.getElementById('video-grid')
+let bgvideo_1 = document.getElementById("bgutton-1")
+let bgvideo_2 = document.getElementById("bgutton-2")
+let bgvideo_3 = document.getElementById("bgutton-3")
+
+bgvideo_1.addEventListener("click", function(){
+    bgvideo_1.classList.add('active')
+    bgvideo_2.classList.remove('active')
+    bgvideo_3.classList.remove('active')
+
+    video_g.setAttribute("src", "https://www.youtube.com/embed/gw_cglJHnFY")
+})
+
+bgvideo_2.addEventListener("click", function(){
+    bgvideo_1.classList.remove('active')
+    bgvideo_2.classList.add('active')
+    bgvideo_3.classList.remove('active')
+
+    video_g.setAttribute("src", "https://www.youtube.com/embed/qrH9vMZBwAk")
+})
+
+bgvideo_3.addEventListener("click", function(){
+    bgvideo_1.classList.remove('active')
+    bgvideo_2.classList.remove('active')
+    bgvideo_3.classList.add('active')
+
+    video_g.setAttribute("src", "https://www.youtube.com/embed/AcTUi59vw_E")    
+})
